04d6baf1a946dc049d7bfc9460a98e27a2e52cd6
FahadQ <Fhdq2003@email.com>
2025-08-31 16:20:47 +0300

    added cat model

--- Cat_Collector_app/models.py
+++ Cat_Collector_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Cat(models.Model):
+    name = models.CharField(120)
+    age = models.IntegerField(MinLengthValidator(0))
+    color = models.CharField(120)
+
+    class Meta:
+        db_table = 'Cat'
+
