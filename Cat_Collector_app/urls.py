6830eb6dcd63c9b41aca5934599f4c33ca5abb3c
FahadQ <Fhdq2003@email.com>
2025-08-31 16:41:12 +0300

    finished list cats and cat details

--- Cat_Collector_app/urls.py
+++ Cat_Collector_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
    path("", views.Home, name="Home"),
-   path("cat/", views.Home, name="Index"),
+   path("cat/", views.Cat_List, name="Index"),
    path("cat/new", views.Home, name="Create"),
+   path("cat/<int:pk>", views.Cat_Details.as_view(), name="cat-details"),
 ]
--- Cat_Collector_app/views.py
+++ Cat_Collector_app/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .models import Cat
 
 # Create your views here.
 
 def Home(request):
 
     return render(request, "home.html")
+
+def Cat_List(request):
+    cats = Cat.objects.all()
+    return render(request, "cat-list.html", {"cats": cats})
+
+
+
+from django.views.generic import (CreateView, DetailView)
+
+class Cat_Details(DetailView):
+    model = Cat
+    template_name = "cat-details.html"
+    context_object_name = "cat"
